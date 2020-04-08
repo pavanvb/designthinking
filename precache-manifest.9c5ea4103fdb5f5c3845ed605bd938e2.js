e86c0bf8e4b2adfcadcdb40720346c91934da057
nachiketkallapur <nachiketkallapur@gmail.com>
2020-04-09 01:08:11 +0530

    Updates

---
+++ precache-manifest.9c5ea4103fdb5f5c3845ed605bd938e2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb605d6b8d0c6ebe023241a6b4965217",
+    "url": "/designthinking/index.html"
+  },
+  {
+    "revision": "50ec509ddbbf2d064475",
+    "url": "/designthinking/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d505745360e29a2187ba",
+    "url": "/designthinking/static/css/main.b5941836.chunk.css"
+  },
+  {
+    "revision": "50ec509ddbbf2d064475",
+    "url": "/designthinking/static/js/2.aa8bfa96.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/designthinking/static/js/2.aa8bfa96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d505745360e29a2187ba",
+    "url": "/designthinking/static/js/main.bc24b20c.chunk.js"
+  },
+  {
+    "revision": "4e06d914022b31d6e0f9",
+    "url": "/designthinking/static/js/runtime-main.6c53709e.js"
+  },
+  {
+    "revision": "4f6b3dae29c9b72c6dca64ca62c894f1",
+    "url": "/designthinking/static/media/img1.4f6b3dae.jpeg"
+  },
+  {
+    "revision": "8c5a438de67a38d0af813cf658b4cb9c",
+    "url": "/designthinking/static/media/img2.8c5a438d.jpeg"
+  },
+  {
+    "revision": "5912d91d830b55e0cd5c7459574aa71b",
+    "url": "/designthinking/static/media/pr.5912d91d.jpg"
+  }
+]);
